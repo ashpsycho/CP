f44d788aeda7134e0edfa68efb9cddfb3784f590
ashpsycho <a1lavania@gmail.com>
2016-04-10 12:56:35 +0800

    codejam begins!

---
+++ codejam/QualificationRounds/2016/inp_gen.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#define T pair<int,int>
+#define x first
+#define y second
+using namespace std;
+int main(){
+	cout<<"1000000\n";
+	for(int i=0;i<1000000;i++)cout<<i<<"\n";
+	return 0;
+}
---
+++ codejam/base_ubuntu.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#define T pair<int,int>
+#define x first
+#define y second
+using namespace std;
+int main(){
+	long long t,te,i,j,k,n,m,ans;
+	cin>>t;
+	for(te=0;te<t;te++){
+		cin>>n;
+		
+		cout<<"Case #"<<(te+1)<<": "<<ans<<"\n";
+	//	printf("Case #%d: %.9Lf\n",te+1,ans);
+
+	}
+	return 0;
+}
